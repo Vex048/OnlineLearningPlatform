58a80e9a3a86f98776a94c697eef76e1768b9a85
Vex048 <borowiak04@gmail.com>
2025-04-14 10:48:35 +0200

    Created index.html (Documentation for REST API)

--- backend/LearningProject/src/main/java/com/example/learningproject/HomeController.java
+++ backend/LearningProject/src/main/java/com/example/learningproject/HomeController.java
@@ -1,14 +1,16 @@
 package com.example.learningproject;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home() {
-        return "<h1>Hello World</h1>";
+        return "index.html";
     }
 }
